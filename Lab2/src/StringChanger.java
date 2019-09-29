d883dd640c7e5a43c6c315fb57791899e5666c26
inaceat <inaceat@gmail.com>
2019-09-29 21:20:34 +0300

    Lab2 2nd task finished.

---
+++ Lab2/src/StringChanger.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+public class StringChanger
+{
+    public static void main(String[] args)
+            throws IOException
+    {
+        //Reading strings
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Enter string:");
+        
+        String readString = reader.readLine();
+        
+        /*
+        (
+            (                   number from 0 to 255:
+                [0-9] |             0 to 9
+                [1-9][0-9] |        10 to 99
+                1[0-9]{2} |         100 to 199
+                2[0-4][0-9] |       200 to 249
+                25[0-5]             250 to 255
+            ) \.                then dot
+        ){3}                    3 times
+        (                       number from 0 to 255 again
+            [0-9] |
+            [1-9][0-9] |
+            1[0-9]{2} |
+            2[0-4][0-9] |
+            25[0-5]
+        )
+        */
+        var ipPattern = Pattern.compile(
+                "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])");
+        
+        System.out.println(ipPattern.matcher(readString).replaceAll("\\${$0}"));
+    }
+}

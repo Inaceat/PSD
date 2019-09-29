ff90dc63742444ded0ad786c30ecc47e4ee05a62
inaceat <inaceat@gmail.com>
2019-09-29 20:41:44 +0300

    Lab2 1st task finished.

--- Lab2/src/StringSorter.java
+++ Lab2/src/StringSorter.java
@@ -1,24 +1,47 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class StringSorter
 {
-
+    
     public static void main(String[] args)
+            throws IOException
     {
-        var arr = args.clone();
-
-        Arrays.sort(arr, (o1, o2) -> o1.length() - o2.length());
-
-        System.out.println("Sort by length:");
-
-        for (var s : arr)
+        //Reading strings
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+        System.out.println("Enter strings:");
+        
+        String readString;
+        var strings = new ArrayList<String>();
+        
+        while (true)
+        {
+            readString = reader.readLine();
+            
+            if (readString.equals(""))
+                break;
+            
+            strings.add(readString);
+        }
+        
+        //Sort by length
+        strings.sort((s1, s2) -> s1.length() - s2.length());
+        
+        System.out.println("Sorted by length:");
+        for (var s : strings)
             System.out.println("  " + s);
-
-        //Arrays.sort(arr, (o1, o2) -> o1.c);
-
-        System.out.println("Sort by lower-case letters count:");
-
-        for (var s : arr)
+        
+        //Sort by lower-case letters number
+        //Assuming numbers are short enough to fit int. Or "toIntExact" will throw "ArithmeticException"
+        strings.sort((s1, s2) -> Math.toIntExact(s1.chars().filter(ch -> Character.isLowerCase(ch)).count() -
+                                                 s2.chars().filter(ch -> Character.isLowerCase(ch)).count()));
+        
+        System.out.println("Sort by lower-case letters number:");
+        for (var s : strings)
             System.out.println("  " + s);
     }
 }
